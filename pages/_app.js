d7ed9a924e593f8217482497627cf4759de34584
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-06-22 12:57:27 +0300

    Chakraprovider WRAP

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
-import Router from 'next/router'; 
+import Router from 'next/router';
 import nProgress from 'nprogress';
 import Head from 'next/head'
-import {ChakraProvider} from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <ChakraProvider>
+          <Layout>
+            <Component {...pageProps}/>
+          </Layout>
+        </ChakraProvider>
+      </Head></>)
 }
 
 export default MyApp
