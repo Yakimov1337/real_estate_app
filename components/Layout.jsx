a56861356dbf7edf184f9a1d03f3003afe306e47
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-06-22 13:27:03 +0300

    Navbar added to layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,17 +1,21 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 
-const Layout = ({children}) => (
+import Navbar from "./Navbar";
+
+const Layout = ({ children }) => (
   <>
     <Head>
       <title>Real Estate</title>
     </Head>
     <Box maxWidth="1280px" m="auto">
-      <header>Navbar</header>
+      <header>
+        <Navbar />
+      </header>
       <main>{children}</main>
       <footer>Footer</footer>
     </Box>
   </>
 );
 
-export default Layout
+export default Layout;
