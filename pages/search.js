370ff1f72be87d3833b85b863b8eb43d3f27b5dc
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-07-01 19:44:46 +0300

    Search content

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import { useRouter } from "next/dist/client/router";
 import Image from 'next/image';
-import {Flex,Box,Text,Icon } from '@chakra-ui/react';
-import {BsFilter} from 'react-icons/bs';
+import { Flex, Box, Text, Icon } from '@chakra-ui/react';
+import { BsFilter } from 'react-icons/bs';
 
 const Search = () => {
-    
+    const [searchFilters, setSearchFitlers] = useState(false);
+    const router = useRouter();
+
+    retrun(
+        <Box>
+            <Flex cursor="pointer" bg="gray.100">
+                <Text>Search Property By Filters</Text>
+            </Flex>
+        </Box>
+    )
 }
+
+export default Search
