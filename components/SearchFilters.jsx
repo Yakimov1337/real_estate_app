950002022ae17de00eb7800925acfe3abbf6ccc7
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-07-02 13:02:44 +0300

    Search filter component

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { Flex, Select, Box, Text, Input, Button } from "@chakra-ui/react";
+import { useRouter } from "next/dist/client/router";
+import { MdCancel } from "react-icons/md";
+import Image from "next/image";
+
+const SearchFilter = () => {
+  return <div></div>;
+};
+
+export default SearchFilter;
